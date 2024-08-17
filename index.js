659e515042c495a056f72635b11f6d18cd584042
shsobur <shsoburhossen951@gmail.com>
2024-08-17 17:16:38 +0600

    JWT veryfi token and admin api added

--- index.js
+++ index.js
@@ -104,21 +104,22 @@ async function run() {
 
   // jwt maiddlewares__
 
-  // const veryfiToken = (req, res, next) => {
-  //   if(!req.headers.authorization) {
-  //     return res.status(401).send({massage: "unsuthorized access"});
-  //   }
+  const veryfiToken = (req, res, next) => {
+    // console.log("Inside veryfied token", req.headers.authorization);
+    
+    if(!req.headers.authorization) {
+      return res.status(401).send({message: "unsuthorized access"});
+    }
 
-  //   const token = req.headers.authorization.split(" ")[1];
-
-  //   jwt.verify(token, process.env.ASSESS_TOKEN_SECRET, (error, decoded) => {
-  //     if(error) {
-  //       return req.status(401).send({massage: "unsuthorized access"})
-  //     }
-  //     req.decoded = decoded;
-  //     next();
-  //   })
-  // }
+    const token = req.headers.authorization.split(" ")[1];
+    jwt.verify(token, process.env.ASSESS_TOKEN_SECRET, (err, decoded) => {
+      if(err) {
+        return res.status(401).send({message: "unsuthorized access"});
+      }
+      req.decoded = decoded;
+      next();
+    })
+  }
 
 
   // Admin maiddlewares__
@@ -136,12 +137,29 @@ async function run() {
 
   // Get oparation for user with veryfi token__
 
-  app.get("/user", async (req, res) => {
+  app.get("/user", veryfiToken, async (req, res) => {
     const result = await userCollection.find().toArray();
     res.send(result);
   })
 
   // Find who is admin__
+
+  app.get("/user/admin/:email", veryfiToken, async (req, res) => {
+    const email = req.params.email;
+    if(email !== req.decoded.email) {
+      console.log("Youe email is match with", req.decoded.email);
+      return res.status(403).send({message: "forbidden access"});
+    }
+    
+    const query = {email: email};
+    const user = await userCollection.findOne(query);
+    let admin = false;
+    if(user) {
+      admin = user?.role === "admin"
+    }
+
+    res.send({admin});
+  })
 
   // app.get("/user/admin/:email" , async (req, res) => {
   //   const email = req.params.email;
